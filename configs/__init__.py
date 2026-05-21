a5ceda5ad37aab753f82c70b7a50d40eaff6302a
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-05-21 09:49:14 +0700

    feat: add __all__ exports to configs __init__

--- configs/__init__.py
+++ configs/__init__.py
@@ -10,3 +10,16 @@ from configs.defaults import (  # noqa: F401
     Stage1Config,
     Stage2Config,
 )
+
+__all__ = [
+    "PATHS",
+    "INFERENCE_DEFAULT_CONFIG",
+    "PREPROCESS_DEFAULT_CONFIG",
+    "STAGE1_DEFAULT_CONFIG",
+    "STAGE2_DEFAULT_CONFIG",
+    "InferenceConfig",
+    "PathsConfig",
+    "PreprocessConfig",
+    "Stage1Config",
+    "Stage2Config",
+]
