f27bb87ba58ff855aac26d11e92b8be2fe8f5b63
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-05-21 09:49:08 +0700

    feat: export gpu_setup functions from training/__init__.py

--- training/__init__.py
+++ training/__init__.py
@@ -1,5 +1,31 @@
-from training.checkpoint import extract_state_dict, load_large_checkpoint, safe_torch_save, safe_torch_save_entity  # noqa: F401
+from training.checkpoint import (  # noqa: F401
+    extract_state_dict,
+    load_large_checkpoint,
+    safe_torch_save,
+    safe_torch_save_entity,
+)
+from training.gpu_setup import (  # noqa: F401
+    cleanup_gpu_cache,
+    detect_gpu_setup,
+    patch_autocast_for_bf16,
+)
 from training.logging_utils import logger, setup_logger  # noqa: F401
 from training.stage1_trainer import main as stage1_main  # noqa: F401
 from training.stage2_trainer import main as stage2_main  # noqa: F401
 from training.train_loop import train_one_epoch, validate  # noqa: F401
+
+__all__ = [
+    "cleanup_gpu_cache",
+    "detect_gpu_setup",
+    "extract_state_dict",
+    "load_large_checkpoint",
+    "logger",
+    "patch_autocast_for_bf16",
+    "safe_torch_save",
+    "safe_torch_save_entity",
+    "setup_logger",
+    "stage1_main",
+    "stage2_main",
+    "train_one_epoch",
+    "validate",
+]
